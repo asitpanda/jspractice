222ea17bde949245a6cf34ecf02611fadc7f6da6
asitpanda <asitpanda87@gmail.com>
2017-12-24 21:14:40 +0530

    removed hoisting code

--- function.js
+++ function.js
@@ -36,14 +36,3 @@ foo.call({id:10})
 
 //O/P =  undefined
 
-foo();
-var foo;
-function foo(){
-    console.log('I am first...');
-}
-
-foo = function(){
-  console.log('No, I am first...');
-}
-
-//O/P = I am first
