1b37a36aa4922a568fdcb6f51d375b5a56765246
asitpanda <asitpanda87@gmail.com>
2017-12-24 22:02:18 +0530

    Update hoisting.js

--- hoisting.js
+++ hoisting.js
@@ -10,3 +10,18 @@ foo = function(){
 }
 
 //O/P = I am first
+
+var bar;
+bar();
+bar = function(){
+  console.log('hello from bar...');
+}
+
+var baz;
+baz();
+function baz(){
+  console.log('hello from baz...');
+}
+
+//O/P =  bar is not a function.
+// if we remove all code related to bar, then o/p will be hello from baz
