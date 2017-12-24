4836d9936e16cdbf30a8e1f171a4dd9986bc6c91
asitpanda <asitpanda87@gmail.com>
2017-12-24 22:05:47 +0530

    calling class with timeout

--- class.js
+++ class.js
@@ -8,3 +8,17 @@ console.log(got instanceof series);
 console.log(got.name)
 
 //O/p =  false, VM266:8 Uncaught TypeError: Cannot read property 'name' of undefined
+
+function Car(model, make) {
+   this.model = model;
+   this.make = make;
+ }
+
+Car.prototype.display = function(){
+  console.log(this.model+' '+ this.make);
+}
+var c1 = new Car('city','honda');
+setTimeout(c1.display,0)
+
+//O/P = undefined undefined
+//Above coded if we remove settimeout then out put will be city honda
