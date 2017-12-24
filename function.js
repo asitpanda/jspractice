648246bcd2a8140665bac1534ba42aaca2695565
asitpanda <asitpanda87@gmail.com>
2017-12-24 22:04:19 +0530

    removed prototype code

--- function.js
+++ function.js
@@ -12,20 +12,6 @@ setTimeout(angular('controllers and services'));
 
 //O/P = angular uses controllers and services
 
-function Car(model, make) {
-   this.model = model;
-   this.make = make;
- }
-
-Car.prototype.display = function(){
-  console.log(this.model+' '+ this.make);
-}
-var c1 = new Car('city','honda');
-setTimeout(c1.display,0)
-
-//O/P = undefined undefined
-//Above coded if we remove settimeout then out put will be city honda
-
 function foo(){
   setTimeout(function(){
     console.log(this.id)
