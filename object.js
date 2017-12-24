bd6e969674bd5b3ba3229e282d3bfb78fe9623c5
asitpanda <asitpanda87@gmail.com>
2017-12-24 22:26:33 +0530

    added array operation

--- object.js
+++ object.js
@@ -13,3 +13,20 @@ var obj2 = {
 obj2.hello()
 
 //o/p = hello samel
+
+var buffer = {
+  entries:[],
+  append:function(s){
+    this.entries.push(s);
+  },
+  concat:function(){
+    console.log(this.entries.join('\\'));
+  }
+};
+var source = ['15','11','2016'];
+source.forEach(buffer.append);
+buffer.concat();
+//o/p = Uncaught TypeError: Cannot read property 'push' of undefined
+//     at append (<anonymous>:4:18)
+//     at Array.forEach (<anonymous>)
+//     at <anonymous>:13:8
